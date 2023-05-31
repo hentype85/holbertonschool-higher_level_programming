a61010c71b2351d8af2aa4f9e66e2bb3a57b53de
martin <martinleiro9@gmail.com>
2023-05-30 19:55:39 -0700

    6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for i in range(0, 9):
+for i in range(0, 10):
     for j in range(i + 1, 10):
         if i == 8 and j == 9:
-            print(f"{i:d}{j:d}")
+            print("89")
         else:
-            print(f"{i:d}{j:d}", end=", ")
+            print("{}{}, ".format(i, j), end="")
