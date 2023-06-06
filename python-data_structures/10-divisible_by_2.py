5193747c97efb79d21800d5ded42688bcd14eb91
Martin <martinleiro9@gmail.com>
2023-06-06 08:59:24 -0700

    div 2

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
+    if my_list == None:
+        return my_list
     newlist = []
     for i in my_list:
         if i % 2 == 0:
             newlist.append(1)
         else:
             newlist.append(0)
-
     return newlist
