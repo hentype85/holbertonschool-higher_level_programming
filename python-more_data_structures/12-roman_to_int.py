736e68b8c8eb652a24220b4e13ab4d85b359478e
martin <martinleiro9@gmail.com>
2023-06-07 14:23:07 -0700

    romanO

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,13 +1,15 @@
 def roman_to_int(roman_string):
-    if roman_string is None or type(roman_string) != str:
+    if type(roman_string) != str:
         return 0
+
     d = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
     Rstr = roman_string
     res = 0
+
     for i in range(len(Rstr)):
-        if Rstr[i] in d:
-            if i < len(Rstr) - 1 and d[Rstr[i]] < d[Rstr[i + 1]]:
-                res -= d[Rstr[i]]
-            else:
-                res += d[Rstr[i]]
+        if i == len(Rstr) - 1 or d[Rstr[i]] >= d[Rstr[i + 1]]:
+            res += d[Rstr[i]]
+        else:
+            res -= d[Rstr[i]]
+
     return res
