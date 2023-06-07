01e02e12d9cf299789d16409b0065b2a5020a18f
martin <martinleiro9@gmail.com>
2023-06-07 14:46:28 -0700

    ay dios

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,12 @@
+#!/usr/bin/python3
 def roman_to_int(roman_string):
+    if type(roman_string) != str:
+        return 0
+
     d = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
     Rs = roman_string
     res = 0
+
     for i in range(len(Rs)):
         if i == len(Rs) - 1 or d.get(Rs[i]) >= d.get(Rs[i + 1]):
             res += d.get(Rs[i])
