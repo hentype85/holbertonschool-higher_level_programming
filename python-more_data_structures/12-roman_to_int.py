49c6763ca77e7375a0cf3081dcf19bf00ce150c7
martin <martinleiro9@gmail.com>
2023-06-07 13:20:03 -0700

    romanox

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if type(roman_string) != str:
-        return
+    if type(roman_string) != str or None == roman_string:
+        return 0
     d = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
     res = 0
     for i in roman_string:
