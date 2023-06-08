96fec509d41fd837429ffb6352e74bdd23e89e0c
Martin <martinleiro9@gmail.com>
2023-06-08 05:20:09 -0700

    max

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary == None or len(a_dictionary) == 0:
+    if a_dictionary == None or len(a_dictionary) == 0 or len(a_dictionary.values()) == 0:
         return None
     m = max(a_dictionary, key=a_dictionary.get)
     return m
