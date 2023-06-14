472cd1b90365a6a7b75c02cdb3f1840bfe180ba8
martin <martinleiro9@gmail.com>
2023-06-13 17:19:04 -0700

    ufff

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -37,10 +37,6 @@ class Square:
     @size.setter
     def size(self, value):
         "set size"
-        if type(value) != int:
-            raise TypeError("size must be an integer")
-        if value < 0:
-            raise ValueError("size must be >= 0")
         self.__size = value
 
     @position.setter
