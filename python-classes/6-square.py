960514e0a2c4b623f4a5acceef1c9f9c7b15a195
martin <martinleiro9@gmail.com>
2023-06-13 17:17:06 -0700

    fix

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -8,15 +8,6 @@ class Square:
     def __init__(self, size=0, position=(0, 0)):
         "Initializes a Square"
 
-        if len(position) != 2:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if type(position[0]) != int:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if type(position[1]) != int:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if position[0] < 0 or position[1] < 0:
-            raise TypeError("position must be a tuple of 2 positive integers")
-
         self.__size = size
         self.__position = position
 
@@ -38,11 +29,19 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-        self.__size = value
 
     @position.setter
     def position(self, value):
         "set position"
+        if len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if type(value[0]) != int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if type(value[1]) != int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+
         self.__position = value
 
     def area(self):
