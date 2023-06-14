828c2aa9774810d35d318f9d073143147bfd1b7a
Martin <martinleiro9@gmail.com>
2023-06-14 10:02:23 -0700

    add integer

--- python-test_driven_development/0-main.py
+++ python-test_driven_development/0-main.py
@@ -6,6 +6,8 @@ print(add_integer(100, -2))
 print(add_integer(2))
 print(add_integer(100.3, -2))
 print(add_integer(float("inf")))
+print(add_integer(123, []))
+
 try:
     print(add_integer(4, "School"))
 except Exception as e:
