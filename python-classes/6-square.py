1b9f15577298ad031170d18827bbdf3696c5aa57
martin <martinleiro9@gmail.com>
2023-06-13 18:21:09 -0700

    fix

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -7,8 +7,8 @@ class Square:
 
     def __init__(self, size=0, position=(0, 0)):
         "Initializes a Square object with a given values"
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
