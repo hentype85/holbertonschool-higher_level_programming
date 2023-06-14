fc063f86d72158774edb57b08e0225099d603a3b
martin <martinleiro9@gmail.com>
2023-06-13 18:23:46 -0700

    ordenando get set

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -15,11 +15,6 @@ class Square:
         """get return size of square"""
         return self.__size
 
-    @property
-    def position(self):
-        """get return position of square"""
-        return self.__position
-
     @size.setter
     def size(self, value):
         "set size"
@@ -28,6 +23,11 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
+    @property
+    def position(self):
+        """get return position of square"""
+        return self.__position
 
     @position.setter
     def position(self, value):
