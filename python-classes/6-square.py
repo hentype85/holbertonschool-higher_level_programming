dd595b9b68ea4608082537f256212dfaac42efed
martin <martinleiro9@gmail.com>
2023-06-13 17:42:26 -0700

    fix2 l

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -3,12 +3,26 @@
 
 
 class Square:
-    """defines a square"""
+    """Square object"""
 
     def __init__(self, size=0, position=(0, 0)):
         "Initializes a Square"
-        self.__size = size
-        self.__position = position
+
+        if len(position) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if type(position[0]) != int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if type(position[1]) != int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if position[0] < 0 or position[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
@@ -23,25 +37,11 @@ class Square:
     @size.setter
     def size(self, value):
         "set size"
-        if type(value) != int:
-            raise TypeError("size must be an integer")
-        if value < 0:
-            raise ValueError("size must be >= 0")
         self.__size = value
 
     @position.setter
     def position(self, value):
         "set position"
-        if type(value) != tuple:
-            raise TypeError('position must be a tuple of 2 positive integers')
-        if len(value) != 2:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if type(value[0]) != int:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if type(value[1]) != int:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if value[0] < 0 or value[1] < 0:
-            raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
     def area(self):
