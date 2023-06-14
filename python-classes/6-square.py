1714835fee39a1b5132c0e633658d554e6f975b0
Martin <martinleiro9@gmail.com>
2023-06-14 05:20:22 -0700

    fix

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -48,13 +48,13 @@ class Square:
         """prints in stdout the square with the character #"""
         if self.__size == 0:
             print()
+        else:
+            for i in range(self.__position[1]):
+                print()
 
-        for i in range(self.__position[1]):
-            print()
-
-        for i in range(self.__size):
-            for j in range(self.__position[0]):
-                print(" ", end="")
-            for k in range(self.__size):
-                print("#", end="")
-            print()
+            for i in range(self.__size):
+                for j in range(self.__position[0]):
+                    print(" ", end="")
+                for k in range(self.__size):
+                    print("#", end="")
+                print()
