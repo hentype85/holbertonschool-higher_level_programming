efb1ee8a46b17593478f060b433a3de4c1c84d4f
martin <martinleiro9@gmail.com>
2023-06-16 15:56:16 -0700

    6rectangle

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -4,11 +4,13 @@
 
 class Rectangle:
     """this represents a rectangle"""
+    number_of_instances = 0
 
     def __init__(self, width=0, height=0):
         """Initializing this rectangle class"""
         self.width = width
         self.height = height
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
