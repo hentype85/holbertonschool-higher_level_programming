95cf3ca985138aee7aa4ce2248367a80ef354029
martin <martinleiro9@gmail.com>
2023-06-19 19:11:31 -0700

    probando

--- python-inheritance/1-main.py
+++ python-inheritance/1-main.py
@@ -5,6 +5,7 @@ my_list = MyList()
 
 my_list.print_sorted([3, 2, 6])
 
+
 print(my_list)
 my_list.print_sorted()
 print(my_list)
--- python-inheritance/README.md
+++ python-inheritance/README.md
@@ -1,1 +1,7 @@
 # Python - Inheritance
+
+```
+python3 -m doctest ./tests/*
+python3 -m doctest -v ./tests/*
+
+```
