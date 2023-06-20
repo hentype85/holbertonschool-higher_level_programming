e5494d8fe4be319d4176785b4f7cbed6f258492a
martin <martinleiro9@gmail.com>
2023-06-20 16:49:05 -0700

    square

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Rectangle subclass Square"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Square(BaseGeometry):
+    """class to define square inherits from BaseGeometry"""
+
+    def __init__(self, size):
+        """intialize a new square"""
+        self.integer_validator("size", size)
+        self.__size = size
