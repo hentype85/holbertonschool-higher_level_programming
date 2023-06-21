8bcb446e0bfa011d4fa1594a517750c234f2809e
martin <martinleiro9@gmail.com>
2023-06-20 18:08:21 -0700

    fix

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""writes to a text file (UTF8) and
-returns the number of characters written
-"""
+"""writes to a text file and return count of characters written"""
+
+
 def write_file(filename="", text=""):
     """writes a string to a text file (UTF8)"""
 
-    with open(filename, "w",encoding="UTF-8") as fd:
+    with open(filename, "w", encoding="UTF-8") as fd:
         return fd.write(text)
