c72f07942ca8802ca936d20d989ca35e7cdd740a
martin <martinleiro9@gmail.com>
2023-06-22 15:01:25 -0700

    savejsonf

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""writes an Object to a text file, using a JSON representation"""
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """writes an Object to a text file using JSON"""
+    with open(filename, "w") as fd:
+        json.dump(my_obj, fd)
