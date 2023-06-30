3ffb9d09baad3c931134d206ffc018b73abde7f7
martin <martinleiro9@gmail.com>
2023-06-30 07:34:42 -0700

    17

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -35,3 +35,11 @@ class Base:
                 for obj in list_objs:
                     jList.append(obj.to_dictionary())
                 fd.write(cls.to_json_string(jList))
+    
+    @staticmethod
+    def from_json_string(json_string):
+        """returns the list of the JSON string representation"""
+        if json_string is None or len(json_string) == 0:
+            return []
+        else:
+            return json.loads(json_string)
