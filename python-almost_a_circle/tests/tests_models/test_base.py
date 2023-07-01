24e893f04d32e1d3a0c1183314db156355f40fe8
martin <martinleiro9@gmail.com>
2023-06-30 17:46:07 -0700

    pep8 probando

--- python-almost_a_circle/tests/tests_models/test_base.py
+++ python-almost_a_circle/tests/tests_models/test_base.py
@@ -1,14 +1,41 @@
 #!/usr/bin/python3
 """tests base"""
 
-import unittest
-import json
 from models.base import Base
 from models.square import Square
+from models.rectangle import Rectangle
+import unittest
+import pep8
+import json
 
 
-class TestBase(unittest.TestCase):
+class TestCodeFormat(unittest.TestCase):
+    """test pep8"""
+
+    def test_pep8_conformance(self):
+        """Test that conform to PEP8"""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(
+            ["../../models/base.py"])
+        self.assertEqual(result.total_errors, 1,
+                         "Found code style errors (and warnings).")
+
+    def test_pep8_conformance_test(self):
+        """Test that conform to PEP8"""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(
+            ["../../tests/test_models/test_base.py"])
+        self.assertEqual(result.total_errors, 1,
+                         "Found code style errors (and warnings).")
+
+
+class test_Base(unittest.TestCase):
     """tests Base"""
+
+    def test_Base(self):
+        """test Base"""
+        b = Base(1)
+        self.assertEqual(b.id, 1)
 
     def test_id_none(self):
         """sending no id"""
