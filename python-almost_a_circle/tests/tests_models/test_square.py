9bdf52d72a57ca6259217696c32b906453c17cd0
martin <martinleiro9@gmail.com>
2023-07-02 18:25:32 -0700

    what

--- python-almost_a_circle/tests/tests_models/test_square.py
+++ python-almost_a_circle/tests/tests_models/test_square.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """tests square"""
 
-from models.base import Base
 from models.square import Square
+from models.rectangle import Rectangle
 import unittest
 import pep8
 import json
@@ -32,8 +32,34 @@ class test_Square(unittest.TestCase):
     """tests Square"""
 
     def test_ok_sq_Subclass(self):
-        self.assertIsInstance(Square(1), Base)
+        self.assertIsInstance(Square(1), Rectangle)
     
     def test_one_and_only_arg(self):
-        s5 = Square(1)
-        self.assertEqual(s5.id, 3)
+        sq = Square(5)
+        self.assertEqual(sq.id, 3)
+    
+    def test_access_and_setter_w(self):
+        with self.assertRaises(AttributeError):
+            Square(1, 5, 5, 98).__width
+    
+    def test_access_and_setter_h(self):
+        with self.assertRaises(AttributeError):
+            Square(1, 5, 5, 98).__height
+    
+    def test_access_and_setter_x(self):
+        with self.assertRaises(AttributeError):
+            Square(1, 5, 5, 98).__x
+    
+    def test_access_and_setter_y(self):
+        with self.assertRaises(AttributeError):
+            Square(1, 5, 5, 98).__y
+
+    def test_access_and_setter_size(self):
+        with self.assertRaises(ValueError):
+            Square(1, 5, 5, 98).size = 0
+    
+    def test_x_notInt(self):
+        with self.assertRaises(TypeError):
+            Square(1, "ABC", 5, 98)
+    
+    
